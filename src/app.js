4a4b75083ece2d5dadf981786989ee9a3ae51fea
César Chavez <cesar.chavez96@icloud.com>
2025-06-13 02:36:40 +0000

    generacion de cartas onload y centrado con CSS

--- src/app.js
+++ src/app.js
@@ -4,4 +4,16 @@ import "./style.css";
 window.onload = function() {
   //write your code here
 
+  let pintas = ["♦", "♥", "♠", "♣"];
+  let valores = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  let randomPintas = pintas[Math.floor(Math.random() * pintas.length)]; 
+  let randomValores = valores[Math.floor(Math.random() * valores.length)];
+  let combinacion = `${randomValores} ${randomPintas}`; 
+
+ 
+const cuerpoCarta = document.querySelector("#carta");
+const contenidoCarta = document.createElement("h1"); 
+
+ contenidoCarta.innerText = combinacion; 
+ cuerpoCarta.append(contenidoCarta);
 };
