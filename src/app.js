a239fed8abf819eb73b20d71de39246232373d56
César Chavez <cesar.chavez96@icloud.com>
2025-06-15 14:45:55 +0000

    fixed card formatting and replaced variables with functions

--- src/app.js
+++ src/app.js
@@ -4,16 +4,23 @@ import "./style.css";
 window.onload = function() {
   //write your code here
 
+  const generadorRandom = (arr) => arr[Math.floor(Math.random() * arr.length)];
   let pintas = ["♦", "♥", "♠", "♣"];
-  let valores = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-  let randomPintas = pintas[Math.floor(Math.random() * pintas.length)]; 
-  let randomValores = valores[Math.floor(Math.random() * valores.length)];
-  let combinacion = `${randomValores} ${randomPintas}`; 
-
+  let valores = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  let cartaValor = generadorRandom(valores);
+  let pintaValor = generadorRandom(pintas);
  
-const cuerpoCarta = document.querySelector("#carta");
-const contenidoCarta = document.createElement("h1"); 
+  const valorCarta = document.querySelector(".valor-carta"); 
+  const pintaValorUp = document.querySelector("#pintaCartaUp");
+  const pintaValorDown = document.querySelector("#pintaCartaDown");
+  const cuerpoCarta = document.querySelector("#carta");
 
- contenidoCarta.innerText = combinacion; 
- cuerpoCarta.append(contenidoCarta);
+  pintaValorUp.innerText = pintaValor; 
+  pintaValorDown.innerText = pintaValor;
+  valorCarta.innerText = cartaValor; 
+
+  if(pintaValor === "♦" || pintaValor === "♥"){
+  pintaValorUp.style.color = "red";
+  pintaValorDown.style.color = "red";
+  }
 };
